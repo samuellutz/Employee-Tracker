8b8a33212e886850c5aba009b4b84547099b2b22
samuel lutz <samlutz89@gmail.com>
2022-01-31 14:22:16 -0800

    updating names and values in seed

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -4,9 +4,9 @@ INSERT into department (name, department_id)
             ('human resources');
 
 insert into roles (title, salary,department_id)
-    values ('project managert', 80000, 1),
-            ('junior software Engineer', 50000, 1),
-            ('senior software Engineer',60000, 1);
+    values ('project manager', 100000, 1),
+            ('junior software Engineer', 70000, 1),
+            ('senior software Engineer',90000, 1);
 
 
 insert into employee_table (first_name, last_name, role_id,manager_id)
