04ac75d38b2ca572d5e01dda94d5e2f10769379d
samuel lutz <samlutz89@gmail.com>
2022-01-31 09:26:18 -0800

    setting base sql for tables

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const mysqli = require("mysqli");
+const mysql = require("mysq12");
 const inquirer = require("inquirer");
 const sql = mysqli.createconnection;
-
-{
-    host: 'localhost',
-    user:'root',
-    password:'root',
-    database:'employee_db'
-}
-console.log('Connected to the employee_db databse')
-)
---
+++ sql/seeds.sql
@@ -0,0 +1,13 @@
+INSERT into department (name, department_id)
+    values ('logistics');
+            ('sales');
+            ('human resources');
+
+insert into roles (title, salary,department_id)
+    values ('project managert', 80000, 1),
+            ('junior software Engineer', 50000, 1),
+            ('senior software Engineer',60000, 1);
+
+
+insert into employee_table (first_name, last_name, role_id,manager_id)
+    values()
