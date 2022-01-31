39f34b9278e8fd752d260229a2d28cd6fade8e9d
samuel lutz <samlutz89@gmail.com>
2022-01-31 11:22:51 -0800

    updating database info

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const sql = mysqli.createconnection;
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'employee_db'
+  });
+  
