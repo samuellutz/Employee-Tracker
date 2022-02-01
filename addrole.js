5df2735a20c1da0ec0955365e5f06651f94a0163
samuel lutz <samlutz89@gmail.com>
2022-02-01 11:09:47 -0800

    simplifying files

---
+++ addrole.js
@@ -0,0 +1,48 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const question = [
+    {
+        type: 'input',
+        message: 'eneter name of new role',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'eneter salary of new role',
+        name: 'salary'
+    },{
+        type: 'input',
+        message: 'eneter department id of new role',
+        name: 'dept_id'
+    }
+];
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user:'root',
+        password:'root',
+        database:'employee_db'
+    }
+)
+
+function addrole() {
+    inquirer.prompt(question)
+    .then(response => {
+        db.query(`instert into roles (title, salary, department_id) values ('${response.name}','${response.salary}','${response.dept_id}')`, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                db.query(`select * from roles;`, (err, result)=>{
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.table(result)
+                })
+            }
+        })
+    })
+}
+
+module.exports = addrole;
--- index.js
+++ index.js
@@ -1,3 +1,68 @@
-const init = require('./initprompt')
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const adddept = require('./adddept')
+const addrole = require('./addrole')
+const db = mysql.createConnection(
+  {
+      host: 'localhost',
+      user:'root',
+      password:'password',
+      database:'employee_db'
+  },
+); 
 
+const question = [
+  {
+    type:'list',
+    message: 'Where would you like to begin',
+    choices: ['View all departments', 'View all roles', 'View all employees', 'Add department', 'Add a role', 'Add an employee', 'Update an employee'],
+    name: 'choice'
+  }
+];
+
+function init() {
+  inquirer
+  .prompt(question)
+  .then(response => {
+    switch (response.choice) {
+        case 'View all departments':
+            db.query('SELECT * FROM department;', (err, result)=> {
+                if (err) {
+                    console.log(err);
+                }
+                console.table(result)
+            });
+            break;
+        case 'View all roles':
+            db.query('SELECT * FROM roles;', (err, result)=> {
+                if (err) {
+                    console.log(err);
+                }
+                console.table(result)
+            });
+            break;
+        case 'View all employees':
+            db.query('SELECT * FROM employee;', (err, result)=> {
+                if (err) {
+                    console.log(err);
+                }
+                console.table(result)
+            });
+            break;
+        case 'Add a department':
+
+            adddept();
+            break;
+        case 'Add a role':
+
+        addrole();
+            break;
+        case 'Update an employee':
+
+            break;
+        }
+    })
+    .catch (err => console.log(err));
+}
 init();
