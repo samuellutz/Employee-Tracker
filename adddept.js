005975577016258a444ff6abc2f64810df34f15f
samuel lutz <samlutz89@gmail.com>
2022-02-01 07:34:30 -0800

    add department function working

---
+++ adddept.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const ctable = require('console.table')
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user:'root',
+        password:'root',
+        database:'employee_db'
+    }
+)
+
+function adddept(){
+    inquirer.prompt({
+        type: 'input',
+        message: 'Enter name of new department',
+        name: 'deptQ'
+    })
+    .then(response=>{
+        db.query(`INSERT INTO department (name) VALUES ('${response.deptQ}')`, (err, result)=>{
+            if (err) {
+                console.log(err);
+            }
+
+            else{
+                db.query('SELECT * FROM department;', (err, result)=> {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.table(result)
+                });
+            }
+        })
+
+    })}
+
+    module.exports = adddept;
