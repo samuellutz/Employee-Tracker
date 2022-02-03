16bb3a8f50aaf3ed20de3ac5a34577223892e0f5
samuel lutz <samlutz89@gmail.com>
2022-02-03 14:47:03 -0800

    trying to merge tables

--- sql/query.sql
+++ sql/query.sql
@@ -1,2 +1,3 @@
-SELECT e.id, e.first_name, e.last_name, r.salary, d.department
-from employee_db e join employee_table on roles = r.salary
+SELECT e.id, e.first_name, e.last_name, r.id, r.salary, d.department
+from (employee_db e join employee_table on roles = r.salary)
+    join department d on 
